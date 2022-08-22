219bfeab7980e222d70757c420c41bc82eb99ba5
sourabh sharma <sourabhsharma@sourabhs-MacBook-Air.local>
2022-08-22 10:20:35 +0530

    #comment practice aws

--- src/App.js
+++ src/App.js
@@ -7,7 +7,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.(By Sourabh Sharma)
+          AWS learn <code>and Practice</code>.(By Sourabh Sharma)
         </p>
         <a
           className="App-link"
